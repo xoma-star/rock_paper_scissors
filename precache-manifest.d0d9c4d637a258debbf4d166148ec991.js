cd1377d6691886b1cbe5be11ceee9a0b1b810ab3
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-04-03 21:22:25 +0500

    Updates

---
+++ precache-manifest.d0d9c4d637a258debbf4d166148ec991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d259b8fb41b39df7165a049f8259b1a0",
+    "url": "/rock_paper_scissors/index.html"
+  },
+  {
+    "revision": "130f09d5240c412d2fc9",
+    "url": "/rock_paper_scissors/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "2eb16d99063ebef526a1",
+    "url": "/rock_paper_scissors/static/css/main.8447535d.chunk.css"
+  },
+  {
+    "revision": "130f09d5240c412d2fc9",
+    "url": "/rock_paper_scissors/static/js/2.29cfdbac.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/rock_paper_scissors/static/js/2.29cfdbac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb16d99063ebef526a1",
+    "url": "/rock_paper_scissors/static/js/main.179104ca.chunk.js"
+  },
+  {
+    "revision": "c775c1854f6fcb1daab2",
+    "url": "/rock_paper_scissors/static/js/runtime-main.49a04f71.js"
+  }
+]);
